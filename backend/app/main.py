adf2620173433441f285d8e2ce65b1d45e5e7c5a
Kinjal-Mehrotra <kinjalmehrotra4@gmail.com>
2025-04-29 10:56:46 +0530

    changed the contents of main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,23 +1,21 @@
 # main.py
 from fastapi import FastAPI
 from routers import (
-    #faculty_router,
-    #labs_router,
-    #syllabus_router,
     mentor_router,
-    #contacts_router,
-    #holidays_router,
+    offices_router,
+    authority_members_router,
+    holidays_router,
+    locations_router
 )
 
 app = FastAPI(title="College Chatbot Backend")
 
 # Include all routers
-#app.include_router(faculty_router.router)
-#app.include_router(labs_router.router)
-#app.include_router(syllabus_router.router)
+app.include_router(authority_members_router.router)
 app.include_router(mentor_router.router)
-#app.include_router(contacts_router.router)
-#app.include_router(holidays_router.router)
+app.include_router(holidays_router)
+app.include_router(offices_router)
+app.include_router(locations_router)
 
 @app.get("/")
 def read_root():
